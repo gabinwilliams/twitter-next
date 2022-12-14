5b4a03de8561efab78231a4384e0e0954e7f14ab
gabinwilliams <scoutsnknives@gmail.com>
2022-12-14 14:59:40 -0600

    css formating in sidebar

--- components/SidebarMenuItem.jsx
+++ components/SidebarMenuItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function SidebarMenuItem({text, Icon}) {
   return (
-    <div>
+    <div className='hoverEffect flex items-center text-gray-700 justify-center xl:justify-start text-lg space-x-3'>
       <Icon className="h-7"/>
       <span>{text}</span>
     </div>
