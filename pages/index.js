80717d78cb9d2fde39b285129930a8988bebf836
gabinwilliams <scoutsnknives@gmail.com>
2022-12-14 20:58:55 -0600

    updated search bar for widget

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Feed from "../components/Feed";
 import Sidebar from "../components/Sidebar";
+import Widgets from "../components/Widgets";
 
 export default function Home() {
     return (
@@ -20,6 +21,7 @@ export default function Home() {
                 {/* feed */}
                 <Feed />
                 {/* widets */}
+                <Widgets/>
             </main>
         </div>
     );
