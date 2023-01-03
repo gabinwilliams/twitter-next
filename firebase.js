d49e1fceb676e87be003fda467e183825065c60c
gabinwilliams <scoutsnknives@gmail.com>
2023-01-03 15:39:51 +0100

    built signin page and completed google provider

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import { getStorage } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.FIREBASE_API_KEY,
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
     authDomain: "twitter-next-app-3b3ef.firebaseapp.com",
     projectId: "twitter-next-app-3b3ef",
     storageBucket: "twitter-next-app-3b3ef.appspot.com",
